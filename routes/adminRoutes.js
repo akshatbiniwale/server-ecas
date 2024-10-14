69f2ca471739655675acd18ee2b964e6f984e8bd
Akshat Biniwale <akshatbiniwale@gmail.com>
2024-10-14 16:30:18 +0530

    Create room backend

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,14 +1,29 @@
 const express = require("express");
-const { createDepartment, registerStudents, createCourse, generateTimetable } = require("../controller/admin");
+const {
+	createDepartment,
+	registerStudents,
+	createCourse,
+	generateTimetable,
+	registerAutoRooms,
+	registerManualRooms,
+} = require("../controller/admin");
 const upload = require("../middleware/multer");
 const router = express.Router();
 
-router.post("/department", createDepartment)
-router.post("/course", upload.single("file"),createCourse)
+router.post("/department", createDepartment);
+router.post("/course", upload.single("file"), createCourse);
 
 //Register students to portal using csv
-router.post("/registerstudents", upload.single("file") ,registerStudents)
+router.post("/registerstudents", upload.single("file"), registerStudents);
 
-router.post("/timetable/create", generateTimetable)
+// resgister rooms to portal using csv
+router.post(
+	"/settings/create-auto-rooms",
+	upload.single("file"),
+	registerAutoRooms
+);
+router.post("/settings/manual-auto-rooms", registerManualRooms);
 
-module.exports = router
+router.post("/timetable/create", generateTimetable);
+
+module.exports = router;
